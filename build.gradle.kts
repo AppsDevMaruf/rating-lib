5a3405c4b3d94590592c653f85b2d23cf8307fba
Maruf Alam <marufssl24@gmail.com>
2025-05-31 13:02:43 +0600

    Initial commit of rating library

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,71 @@
-import com.chalo.buildsrc.Modules
-
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.ksp)
-    alias(libs.plugins.kotlinSerialization)
+    id("com.android.library") version "8.3.2"  // Update to latest stable version
+    id("org.jetbrains.kotlin.android") version "1.9.20"  // Update Kotlin version
 }
 
-apply(from = "$rootDir/base-module.gradle")
 
 android {
-    namespace = "com.chalo.ratting"
+    namespace = "com.chalo.rating"  // Fixed typo in "rating"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"  // Updated to match Kotlin version
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+    publishing {
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/AppsDevMaruf/rating-lib")
+                credentials {
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                }
+            }
+        }
+    }
+    
 }
 
+
 dependencies {
-    implementation(project(Modules.CORE))
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation(platform("androidx.compose:compose-bom:2025.05.01"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
+
+// Optional: Add this if you want to publish to Maven Local
+apply(plugin = "maven-publish")
